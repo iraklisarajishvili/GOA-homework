e27042871d0e78c4f4488c7c02884ee19f8f9d0f
iraklisarajishvili <iraklisarajishvili8@gmail.com>
2025-11-30 21:56:30 +0400

    second commit

---
+++ day 09/classwork/cw1.py
@@ -0,0 +1,4 @@
+# 1)კომენტარის სახით ახსენით განსხვავება explicit და implicit data conversion შორის და დაწერე მაგალითები
+
+
+# explicit data conversion ნიშნავს იმას რომ მოქმედებებს ჩვენთვითონ ვცვლით და implicit data conversion როცა კომპიუტერი თვითონ ცვლის მონაცემებს
---
+++ day 09/classwork/cw4.py
@@ -0,0 +1,6 @@
+# 4)დაწერე True იქნება თუ False
+# number = 6.0
+# number > 6
+
+
+# იქნება false
